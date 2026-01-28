cb83d08411d517817e8003cab1d56863af726d6b
OpenLucusKaka <zhongwenjun@ricent.com>
2026-01-28 18:01:50 +0800

    fix:解决循环引用

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from . import auth  # noqa: E402, F401
-from . import message  # noqa: E402, F401
-from . import health  # noqa: E402, F401
 
 auth_bp = Blueprint("auth", __name__)
 message_bp = Blueprint("message", __name__)
 health_bp = Blueprint("health", __name__)
+
+from . import auth  # noqa: E402, F401
+from . import message  # noqa: E402, F401
+from . import health  # noqa: E402, F401
